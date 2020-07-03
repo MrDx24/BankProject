f051b0a980e60bda6686ac6717c023f533388c0a
Deep Patadiya <36883468+MrDx24@users.noreply.github.com>
2020-07-03 13:54:00 +0530

    addpayee updated

--- src/com/test/Test.java
+++ src/com/test/Test.java
@@ -2,6 +2,7 @@ package com.test;
 
 import java.time.LocalDateTime;
 
+import com.dao.AddPayeeDao;
 import com.dao.RegisterDao;
 import com.model.Customer;
 import com.model.Register;
@@ -9,20 +10,21 @@ import com.model.Register;
 public class Test {
 
 	public static void main(String[] args) {
-		LocalDateTime currentTime = LocalDateTime.now();
-		String date = currentTime.toString();
-		Customer c = new Customer("Daneil", "DRon","McKooger", "Male","12/10/1999","Maharashtra", "Pune", "Kasba Peth", 411004, "ryan12@gmail.com", "9128734655", 
-				"928314567834", "TGKKKW7856C", "Indian");
-		Register r = new Register(c,"Savings","ry12", "ry1234", date);
-		RegisterDao dao = new RegisterDao();
-		int i = dao.setDetails(r);
-		if(i==0) {
-			System.out.println("Successful");
+//		LocalDateTime currentTime = LocalDateTime.now();
+//		String date = currentTime.toString();
+//		Customer c = new Customer("Daneil", "DRon","McKooger", "Male","12/10/1999","Maharashtra", "Pune", "Kasba Peth", 411004, "ryan12@gmail.com", "9128734655", 
+//				"928314567834", "TGKKKW7856C", "Indian");
+//		Register r = new Register(c,"Savings","ry12", "ry1234", date);
+//		RegisterDao dao = new RegisterDao();
+		AddPayeeDao dao = new AddPayeeDao();
+		try {
+			int i = dao.addPayee(1000000008);
+			System.out.println("ans : " + i);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		else {
-			System.out.println("not");
-		}
-
+		
 	}
 
 }
